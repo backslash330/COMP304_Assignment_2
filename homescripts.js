9d12e267a94adce67019ebe331e435a184dee917
Nicholas Almeida <n.almeida330@gmail.com>
2022-01-10 15:08:23 -0700

    added stop on checking squares after win

--- homescripts.js
+++ homescripts.js
@@ -15,25 +15,31 @@ var turncounter = 0;
 $(document).ready(function(){
 
     $(".col").click(function(){
-        if($(this).hasClass("x") || $(this).hasClass("o")){
-            alert("This box is already taken");
+        if(checkForWin() == true){
+            alert("The game is over! Refresh the page to play again!");
         }
         else{
-            if(turncounter % 2 === 0){
-                $(this).addClass("x");
-                turncounter++;
+            if($(this).hasClass("x") || $(this).hasClass("o")){
+                alert("This box is already taken");
             }
             else{
-                $(this).addClass("o");
-                turncounter++;
+                if(turncounter % 2 === 0){
+                    $(this).addClass("x");
+                    turncounter++;
+                }
+                else{
+                    $(this).addClass("o");
+                    turncounter++;
+                }
+                winnerAlert();
             }
-            checkForWinner();
         }
+
     });
 
 });
 
-function checkForWinner(){
+function winnerAlert(){
     /*
     horizonal wins are col00, col01, col02
     horizontal wins are col10, col11, col12
@@ -101,5 +107,41 @@ function checkForWinner(){
     else if(turncounter === 9){
         alert("It's a tie!");
     }
-    
+
 };
+
+function checkForWin(){
+    // check for horizontal wins of x
+    if($(".col00").hasClass("x") && $(".col01").hasClass("x") && $(".col02").hasClass("x")){
+        return true;
+    }
+    else if($(".col10").hasClass("x") && $(".col11").hasClass("x") && $(".col12").hasClass("x")){
+        return true;
+    }
+    else if($(".col20").hasClass("x") && $(".col21").hasClass("x") && $(".col22").hasClass("x")){
+        return true;
+    }
+    // check for vertical wins of x
+    else if($(".col00").hasClass("x") && $(".col10").hasClass("x") && $(".col20").hasClass("x")){
+        return true;
+    }
+    else if($(".col01").hasClass("x") && $(".col11").hasClass("x") && $(".col21").hasClass("x")){
+        return true;
+    }
+    else if($(".col02").hasClass("x") && $(".col12").hasClass("x") && $(".col22").hasClass("x")){
+        return true;
+    }
+    // check for diagonal wins of x
+    else if($(".col00").hasClass("x") && $(".col11").hasClass("x") && $(".col22").hasClass("x")){
+        return true;
+    }
+    else if($(".col20").hasClass("x") && $(".col11").hasClass("x") && $(".col02").hasClass("x")){
+        return true;
+    }
+    // check for horizontal wins of o
+    else if($(".col00").hasClass("o") && $(".col01").hasClass("o") && $(".col02").hasClass("o")){
+        return true;
+    }
+    else if($(".col10").hasClass("o") && $(".col11").hasClass("o") && $(".col12").hasClass("o")){
+        return true;
+    }}
